d690ebe9d43d6ae6f30734a99e2ebab038ad188f
Michal Simek <michal.simek@xilinx.com>
2015-01-07 10:04:53 +0100

    cdns_uart: Fix indentation in header
    Signed-off-by: Michal Simek <michal.simek@xilinx.com>

--- include/drivers/cadence/cdns_uart.h
+++ include/drivers/cadence/cdns_uart.h
@@ -35,19 +35,19 @@
 /* This is very minimalistic and will only work in QEMU.  */
 
 /* CADENCE Registers */
-#define R_UART_CR    (0x00)
-#define R_UART_IER   (0x08)
-#define R_UART_IDR   (0x0C)
-#define R_UART_IMR   (0x10)
-#define R_UART_CISR  (0x14)
-#define R_UART_RTRIG (0x20)
-#define R_UART_SR    (0x2C)
-#define UART_SR_INTR_RTRIG     0x00000001
-#define UART_SR_INTR_REMPTY    0x00000002
-#define UART_SR_INTR_TEMPTY    0x00000008
-#define UART_SR_INTR_TFUL      0x00000010
+#define R_UART_CR	(0x00)
+#define R_UART_IER	(0x08)
+#define R_UART_IDR	(0x0C)
+#define R_UART_IMR	(0x10)
+#define R_UART_CISR	(0x14)
+#define R_UART_RTRIG	(0x20)
+#define R_UART_SR	(0x2C)
+#define UART_SR_INTR_RTRIG	0x00000001
+#define UART_SR_INTR_REMPTY	0x00000002
+#define UART_SR_INTR_TEMPTY	0x00000008
+#define UART_SR_INTR_TFUL	0x00000010
 
-#define R_UART_TX  (0x30)
-#define R_UART_RX  (0x30)
+#define R_UART_TX	(0x30)
+#define R_UART_RX	(0x30)
 
 #endif
