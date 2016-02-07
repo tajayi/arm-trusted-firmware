f1e48dd28fdeffa9a77f129091438fac6ceb5956
Soren Brinkmann <soren.brinkmann@xilinx.com>
2016-02-07 13:18:46 -0800

    zynqmp: Remove DEBUG_XLAT_TABLE conditional
    The DEBUG_XLAT_TABLE is obsolete. Remove all conditionals using it.
    
    Acked-by: Michal Simek <michal.simek@xilinx.com>
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>

--- plat/xilinx/zynqmp/include/platform_def.h
+++ plat/xilinx/zynqmp/include/platform_def.h
@@ -39,9 +39,7 @@
  ******************************************************************************/
 
 /* Size of cacheable stacks */
-#if DEBUG_XLAT_TABLE
-#define PLATFORM_STACK_SIZE 0x800
-#elif IMAGE_BL31
+#if IMAGE_BL31
 #define PLATFORM_STACK_SIZE 0x400
 #elif IMAGE_BL32
 #define PLATFORM_STACK_SIZE 0x440
