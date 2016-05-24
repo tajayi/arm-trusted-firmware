tajayi/arm-trusted-firmware

commit fcdfb2d80875c06c5e1147fb543ea6357b172311

zynqmp: Reduce mapped memory area
The GIC area is specified larger than it needs to be and can be reduced.
Which allows reducing the structures required for the translation tables
as well.
This results in a reduction of memory footprint of ca. 4k.

Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
Acked-by: Michal Simek <michal.simek@xilinx.com>

plat/xilinx/zynqmp/include/platform_def.h
@@ -100,8 +100,8 @@
  * Platform specific page table and MMU setup constants
  ******************************************************************************/
 #define ADDR_SPACE_SIZE			(1ull << 32)
-#define MAX_XLAT_TABLES			5
-#define MAX_MMAP_REGIONS		7
+#define MAX_XLAT_TABLES			4
+#define MAX_MMAP_REGIONS		6
 
 #define CACHE_WRITEBACK_SHIFT   6
 #define CACHE_WRITEBACK_GRANULE (1 << CACHE_WRITEBACK_SHIFT)

plat/xilinx/zynqmp/zynqmp_def.h
@@ -47,7 +47,7 @@
 #define DEVICE0_BASE		0xFF000000
 #define DEVICE0_SIZE		0x00E00000
 #define DEVICE1_BASE		0xF9000000
-#define DEVICE1_SIZE		0x01000000
+#define DEVICE1_SIZE		0x00800000
 
 /* For cpu reset APU space here too 0xFE5F1000 CRF_APB*/
 #define CRF_APB_BASE		0xFD1A0000

commit 9a4ac9c640be469143a6d172532c2c6ab3966828

zynqmp: Increase number of xlat tables for BL32
Commit fcdfb2d80875 ("zynqmp: Reduce mapped memory area") reduced
the number of xlat tables resulting in BL32 hitting an assertion:
  ASSERT: init_xlation_table <271> : next_xlat <= MAX_XLAT_TABLES

Fixes: fcdfb2d80875 ("zynqmp: Reduce mapped memory area")
Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>

plat/xilinx/zynqmp/include/platform_def.h
@@ -100,8 +100,12 @@
  * Platform specific page table and MMU setup constants
  ******************************************************************************/
 #define ADDR_SPACE_SIZE			(1ull << 32)
-#define MAX_XLAT_TABLES			4
 #define MAX_MMAP_REGIONS		6
+#if IMAGE_BL32
+# define MAX_XLAT_TABLES		5
+#else
+# define MAX_XLAT_TABLES		4
+#endif
 
 #define CACHE_WRITEBACK_SHIFT   6
 #define CACHE_WRITEBACK_GRANULE (1 << CACHE_WRITEBACK_SHIFT)