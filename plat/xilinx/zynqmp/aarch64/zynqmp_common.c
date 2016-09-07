tajayi/arm-trusted-firmware

commit 17fd109dbafb84a8365fbf189c87090944012809

zynqmp: Add support to provide silicon id through SMC
Add support to provide silicon id to Non secure
software through SMC.

Signed-off-by: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
---
Changes for v3:
- Rebased on top of latest master
Changes for v2:
- Removed parenthesis as per review comment

plat/xilinx/zynqmp/aarch64/zynqmp_common.c
@@ -140,7 +140,7 @@ static const struct {
 	},
 };
 
-static unsigned int zynqmp_get_silicon_id(void)
+unsigned int zynqmp_get_silicon_id(void)
 {
 	uint32_t id;
 

plat/xilinx/zynqmp/pm_service/pm_defs.h
@@ -89,6 +89,7 @@ enum pm_api_id {
 	PM_INIT,
 	PM_FPGA_LOAD,
 	PM_FPGA_GET_STATUS,
+	PM_GET_CHIPID,
 	PM_API_MAX
 };
 

plat/xilinx/zynqmp/pm_service/pm_svc_main.c
@@ -320,6 +320,9 @@ uint64_t pm_smc_handler(uint32_t smc_fid, uint64_t x1, uint64_t x2, uint64_t x3,
 		SMC_RET1(handle, (uint64_t)ret | ((uint64_t)value) << 32);
 	}
 
+	case PM_GET_CHIPID:
+		SMC_RET1(handle, zynqmp_get_silicon_id());
+
 	default:
 		WARN("Unimplemented PM Service Call: 0x%x\n", smc_fid);
 		SMC_RET1(handle, SMC_UNK);

plat/xilinx/zynqmp/zynqmp_private.h
@@ -39,6 +39,7 @@ void zynqmp_config_setup(void);
 unsigned int zynqmp_get_uart_clk(void);
 int zynqmp_is_pmu_up(void);
 unsigned int zynqmp_get_bootmode(void);
+unsigned int zynqmp_get_silicon_id(void);
 
 /*
  * Register handler to specific GIC entrance

commit b2a9153d08c3a8eed953055436306f5aadd1c66a

zynqmp: Fix build errors at low log levels
Commit 17fd109dbafb ("zynqmp: Add support to provide silicon id
through SMC") added a new user for zynqmp_get_silicon_id without
moving that function out of the LOG_LEVEL dependent compile guards,
which results in build errors at low log levels.

Fixes: 17fd109dbafb ("zynqmp: Add support to provide silicon id through SMC")
Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
Acked-by: Michal Simek <michal.simek@xilinx.com>

plat/xilinx/zynqmp/aarch64/zynqmp_common.c
@@ -89,6 +89,18 @@ static unsigned int zynqmp_get_system_timer_freq(void)
 	return 100000000;
 }
 
+unsigned int zynqmp_get_silicon_id(void)
+{
+	uint32_t id;
+
+	id = mmio_read_32(ZYNQMP_CSU_BASEADDR + ZYNQMP_CSU_IDCODE_OFFSET);
+
+	id &= ZYNQMP_CSU_IDCODE_DEVICE_CODE_MASK | ZYNQMP_CSU_IDCODE_SVD_MASK;
+	id >>= ZYNQMP_CSU_IDCODE_SVD_SHIFT;
+
+	return id;
+}
+
 #if LOG_LEVEL >= LOG_LEVEL_NOTICE
 static const struct {
 	unsigned int id;
@@ -139,18 +151,6 @@ static const struct {
 		.name = "17EG",
 	},
 };
-
-unsigned int zynqmp_get_silicon_id(void)
-{
-	uint32_t id;
-
-	id = mmio_read_32(ZYNQMP_CSU_BASEADDR + ZYNQMP_CSU_IDCODE_OFFSET);
-
-	id &= ZYNQMP_CSU_IDCODE_DEVICE_CODE_MASK | ZYNQMP_CSU_IDCODE_SVD_MASK;
-	id >>= ZYNQMP_CSU_IDCODE_SVD_SHIFT;
-
-	return id;
-}
 
 static char *zynqmp_get_silicon_idcode_name(void)
 {